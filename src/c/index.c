00914b11483281c44afe66fa89314a242c4d0e5d
1129278 <a1129278@univie.ac.at>
2018-07-08 15:31:43 +0200

    BackUp

---
+++ src/c/index.c
@@ -0,0 +1,147 @@
+#include "pebble.h"
+
+#define REPEAT_INTERVAL_MS 50
+
+// This is a custom defined key for saving our count field
+#define NUM_DRINKS_PKEY 1
+
+// You can define defaults for values in persistent storage
+#define NUM_DRINKS_DEFAULT 0
+
+static Window *s_main_window;
+
+static ActionBarLayer *s_action_bar;
+static TextLayer *s_header_layer, *s_body_layer, *s_label_layer;
+static GBitmap *s_icon_plus, *s_icon_minus;
+
+static int s_num_drinks = NUM_DRINKS_DEFAULT;
+
+static void update_text() {
+  static char s_body_text[18];
+  
+  if (s_num_drinks == 0) {
+    text_layer_set_text(s_header_layer, "Pff, nüchtern...");
+  } else if (s_num_drinks == 1) {
+    text_layer_set_text(s_header_layer, "Bist Fahrer, hm?");
+  } else if (s_num_drinks == 2) {
+    text_layer_set_text(s_header_layer, "Doch kein Fahrer...");
+  } else if (s_num_drinks == 3) {
+    text_layer_set_text(s_header_layer, "Party started!");
+  } else if (s_num_drinks == 4) {
+    text_layer_set_text(s_header_layer, "Eins geht noch!");
+  } else if (s_num_drinks == 5) {
+    text_layer_set_text(s_header_layer, "Spiegel halten!");
+  } else if (s_num_drinks == 6) {
+    text_layer_set_text(s_header_layer, "Ruhig, Brauner!");
+  } else if (s_num_drinks == 7) {
+    text_layer_set_text(s_header_layer, "Trink Wasser!");
+  } else if (s_num_drinks == 8) {
+    text_layer_set_text(s_header_layer, "Hol' nen Kübel.");
+  } else if (s_num_drinks == 9) {
+    text_layer_set_text(s_header_layer, "Wer hält die Haare?");
+  } else {
+    text_layer_set_text(s_header_layer, "Ruf die Rettung!");
+  }
+  
+  if (s_num_drinks == 1) {
+    snprintf(s_body_text, sizeof(s_body_text), "%u Bier", s_num_drinks);
+  } else {
+    snprintf(s_body_text, sizeof(s_body_text), "%u Biere", s_num_drinks);
+  }
+  
+  text_layer_set_text(s_body_layer, s_body_text);
+}
+
+static void increment_click_handler(ClickRecognizerRef recognizer, void *context) {
+  s_num_drinks++;
+  update_text();
+}
+
+static void decrement_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if (s_num_drinks <= 0) {
+    // Keep the counter at zero
+    return;
+  }
+
+  s_num_drinks--;
+  update_text();
+}
+
+static void click_config_provider(void *context) {
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, REPEAT_INTERVAL_MS, increment_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, REPEAT_INTERVAL_MS, decrement_click_handler);
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+
+  s_action_bar = action_bar_layer_create();
+  action_bar_layer_add_to_window(s_action_bar, window);
+  action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
+
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_icon_plus);
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_DOWN, s_icon_minus);
+
+  int width = layer_get_frame(window_layer).size.w - ACTION_BAR_WIDTH - 3;
+
+  s_header_layer = text_layer_create(GRect(4, PBL_IF_RECT_ELSE(0, 30), width, 60));
+  text_layer_set_font(s_header_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_background_color(s_header_layer, GColorClear);
+  text_layer_set_text(s_header_layer, "LALA");
+  text_layer_set_text_alignment(s_header_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+  layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
+
+  s_body_layer = text_layer_create(GRect(4, PBL_IF_RECT_ELSE(44, 60), width, 60));
+  text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_background_color(s_body_layer, GColorClear);
+  text_layer_set_text_alignment(s_body_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+  layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
+
+  s_label_layer = text_layer_create(GRect(4, PBL_IF_RECT_ELSE(44, 60) + 28, width, 60));
+  text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_background_color(s_label_layer, GColorClear);
+  text_layer_set_text(s_label_layer, "intus");
+  text_layer_set_text_alignment(s_label_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+  layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
+
+  update_text();
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_header_layer);
+  text_layer_destroy(s_body_layer);
+  text_layer_destroy(s_label_layer);
+
+  action_bar_layer_destroy(s_action_bar);
+}
+
+static void init() {
+  s_icon_plus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_PLUS);
+  s_icon_minus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_MINUS);
+
+  // Get the count from persistent storage for use if it exists, otherwise use the default
+  s_num_drinks = persist_exists(NUM_DRINKS_PKEY) ? persist_read_int(NUM_DRINKS_PKEY) : NUM_DRINKS_DEFAULT;
+
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  // Save the count into persistent storage on app exit
+  persist_write_int(NUM_DRINKS_PKEY, s_num_drinks);
+
+  window_destroy(s_main_window);
+
+  gbitmap_destroy(s_icon_plus);
+  gbitmap_destroy(s_icon_minus);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
